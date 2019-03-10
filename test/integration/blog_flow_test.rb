7c8dc2551da39ba2245176a77f79379bc34ce74d
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-10 09:12:17 +0400

    Testing redirects logic removed from integration tests.

--- test/integration/blog_flow_test.rb
+++ test/integration/blog_flow_test.rb
@@ -1,34 +1,27 @@
 require 'test_helper'
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
-  test "can create an article" do
-    get new_article_path
-    assert_response :success
-
+  test 'can create an article' do
+    title = 'Title'
+    body = 'Body'
     post articles_path,
-         params: { article: { title: "New one", body: "Something interesting" } }
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
+         params: { article: { title: title, body: body } }
+
+    new_article = Article.find_by(title: title)
+    assert_equal body, new_article.body
   end
 
-  test "can update an article" do
+  test 'can update an article' do
     article = articles(:one)
-    get edit_article_path(article)
-    assert_response :success
-
-    new_title = "New title"
+    new_title = 'New title'
     put article_path(article),
         params: { article: { title: new_title } }
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
 
-    updated_article = Article.find(article.id)
-    assert_equal new_title, updated_article.title
+    article.reload
+    assert_equal new_title, article.title
   end
 
-  test "can delete an article" do
+  test 'can delete an article' do
     article = articles(:one)
     delete article_path(article)
 
